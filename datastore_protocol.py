a990cffb90f9bf173fe4aac6661b1f0bb2bde911
EpicUsername12 <39063367+EpicUsername12@users.noreply.github.com>
2023-06-15 12:02:00 +0200

    Implement:
    - DataStore::PrepareUpdateObject
    - DataStore::CompleteUpdateObject
    - DataStore::SearchObject
    - DataStore::RateObject
    - DataStore::GetObjectInfos

--- datastore_protocol.py
+++ datastore_protocol.py
@@ -2,60 +2,7 @@ from nintendo.nex import datastore, rmc, common
 from pymongo.collection import Collection
 from typing import Callable
 import datetime
-
-
-class MK8DataStoreChangeMetaParam(common.Structure):
-    def __init__(self):
-        super().__init__()
-        self.data_id = None
-        self.modifies_flag = None
-        self.name = None
-        self.permission = datastore.DataStorePermission()
-        self.delete_permission = datastore.DataStorePermission()
-        self.period = None
-        self.meta_binary = None
-        self.tags = None
-        self.update_password = None
-        self.referred_count = None
-        self.data_type = None
-        self.status = None
-        self.compare_param = datastore.DataStoreChangeMetaCompareParam()
-
-    def check_required(self, settings, version):
-        for field in ['data_id', 'modifies_flag', 'name', 'period', 'meta_binary', 'tags', 'update_password', 'referred_count', 'data_type', 'status']:
-            if getattr(self, field) is None:
-                raise ValueError("No value assigned to required field: %s" % field)
-
-    def load(self, stream, version):
-        self.data_id = stream.u64()
-        self.modifies_flag = stream.u32()
-        self.name = stream.string()
-        self.permission = stream.extract(datastore.DataStorePermission)
-        self.delete_permission = stream.extract(datastore.DataStorePermission)
-        self.period = stream.u16()
-        self.meta_binary = stream.qbuffer()
-        self.tags = stream.list(stream.string)
-        self.update_password = stream.u64()
-        self.referred_count = stream.u32()
-        self.data_type = stream.u16()
-        self.status = stream.u8()
-        self.compare_param = stream.extract(datastore.DataStoreChangeMetaCompareParam)
-
-    def save(self, stream, version):
-        self.check_required(stream.settings, version)
-        stream.u64(self.data_id)
-        stream.u32(self.modifies_flag)
-        stream.string(self.name)
-        stream.add(self.permission)
-        stream.add(self.delete_permission)
-        stream.u16(self.period)
-        stream.qbuffer(self.meta_binary)
-        stream.list(self.tags, stream.string)
-        stream.u64(self.update_password)
-        stream.u32(self.referred_count)
-        stream.u16(self.data_type)
-        stream.u8(self.status)
-        stream.add(self.compare_param)
+import pymongo
 
 
 class CommonDataStoreServer(datastore.DataStoreServer):
@@ -67,8 +14,8 @@ class CommonDataStoreServer(datastore.DataStoreServer):
                  datastore_db: Collection,
                  sequence_db: Collection,
                  head_object_by_key: Callable[[str], tuple[bool, int, str]],
-                 calculate_s3_object_key: Callable[[Collection, rmc.RMCClient, int], str],
-                 calculate_s3_object_key_ex: Callable[[Collection, int, int], str]):
+                 calculate_s3_object_key: Callable[[Collection, rmc.RMCClient, int, int], str],
+                 calculate_s3_object_key_ex: Callable[[Collection, int, int, int], str]):
         super().__init__()
         self.settings = settings
         self.s3_client = s3_client
@@ -87,6 +34,8 @@ class CommonDataStoreServer(datastore.DataStoreServer):
 
     def validate_prepare_post_param(self, client, param: datastore.DataStorePreparePostParam):
         return True
+
+    # ==================================================================================
 
     async def prepare_post_object(self, client, param: datastore.DataStorePreparePostParam) -> datastore.DataStoreReqPostInfo:
 
@@ -136,7 +85,9 @@ class CommonDataStoreServer(datastore.DataStoreServer):
 
         doc.update({"ratings": ratings})
 
-        s3_key = self.calculate_s3_object_key(self.datastore_db, client, param.persistence_init_param.persistence_id)
+        self.datastore_db.insert_one(doc)
+
+        s3_key = self.calculate_s3_object_key(self.datastore_db, client, param.persistence_init_param.persistence_id, doc["id"])
         response = self.s3_client.generate_presigned_post(Bucket=self.s3_bucket,
                                                           Key=s3_key,
                                                           ExpiresIn=(15 * 60),
@@ -189,8 +140,6 @@ class CommonDataStoreServer(datastore.DataStoreServer):
             field_signature
         ]
 
-        self.datastore_db.insert_one(doc)
-
         return res
 
     async def complete_post_object(self, client, param: datastore.DataStoreCompletePostParam):
@@ -198,12 +147,90 @@ class CommonDataStoreServer(datastore.DataStoreServer):
             datastore_object = self.datastore_db.find_one({"id": param.data_id})
             if datastore_object and (client.pid() == datastore_object["owner"]):
                 persistence_id = datastore_object["persistence_id"]
-                success, _, _ = self.head_object_by_key(self.calculate_s3_object_key(self.datastore_db, client, persistence_id))
+                success, _, _ = self.head_object_by_key(self.calculate_s3_object_key(self.datastore_db, client, persistence_id, param.data_id))
                 if success:
                     self.datastore_db.update_one({"id": param.data_id}, {"$set": {"is_validated": True}})
 
+    async def prepare_update_object(self, client, param: datastore.DataStorePrepareUpdateParam):
+
+        obj = self.datastore_db.find_one({"id": param.data_id})
+        if not obj:
+            raise common.RMCError("DataStore::NotFound")
+
+        if client.pid() != obj["owner"]:
+            raise common.RMCError("DataStore::PermissionDenied")
+
+        s3_key = self.calculate_s3_object_key(self.datastore_db, client, obj["persistence_id"], param.data_id)
+        response = self.s3_client.generate_presigned_post(Bucket=self.s3_bucket,
+                                                          Key=s3_key,
+                                                          ExpiresIn=(15 * 60),
+                                                          Conditions=[["content-length-range", param.size, param.size], {"x-amz-security-token": ""}])
+
+        fields = response["fields"]
+
+        res = datastore.DataStoreReqUpdateInfo()
+        res.url = "https://%s.%s" % (self.s3_bucket, self.s3_endpoint_domain)
+        res.form = []
+        res.headers = []
+        res.root_ca_cert = b""
+        res.version = 2
+
+        field_key = datastore.DataStoreKeyValue()
+        field_key.key = "key"
+        field_key.value = fields["key"]
+
+        field_credential = datastore.DataStoreKeyValue()
+        field_credential.key = "X-Amz-Credential"
+        field_credential.value = fields["x-amz-credential"]
+
+        field_date = datastore.DataStoreKeyValue()
+        field_date.key = "X-Amz-Date"
+        field_date.value = fields["x-amz-date"]
+
+        field_security_token = datastore.DataStoreKeyValue()
+        field_security_token.key = "X-Amz-Security-Token"
+        field_security_token.value = ""
+
+        field_algorithm = datastore.DataStoreKeyValue()
+        field_algorithm.key = "X-Amz-Algorithm"
+        field_algorithm.value = fields["x-amz-algorithm"]
+
+        field_policy = datastore.DataStoreKeyValue()
+        field_policy.key = "policy"
+        field_policy.value = fields["policy"]
+
+        field_signature = datastore.DataStoreKeyValue()
+        field_signature.key = "X-Amz-Signature"
+        field_signature.value = fields["x-amz-signature"]
+
+        res.form = [
+            field_key,
+            field_credential,
+            field_security_token,
+            field_algorithm,
+            field_date,
+            field_policy,
+            field_signature
+        ]
+
+        self.datastore_db.update_one({"id": param.data_id}, {"$set": {"is_validated": False, "update_size": param.size}})
+
+        return res
+
+    async def complete_update_object(self, client, param: datastore.DataStoreCompleteUpdateParam):
+        if param.success:
+            datastore_object = self.datastore_db.find_one({"id": param.data_id})
+            if datastore_object and (client.pid() == datastore_object["owner"]):
+                persistence_id = datastore_object["persistence_id"]
+                success, _, _ = self.head_object_by_key(self.calculate_s3_object_key(self.datastore_db, client, persistence_id, param.data_id))
+                if success:
+                    self.datastore_db.update_one({"id": param.data_id}, {"$set": {"is_validated": True, "size": datastore_object["update_size"]}})
+
     async def prepare_get_object(self, client, param: datastore.DataStorePrepareGetParam) -> datastore.DataStoreReqGetInfo:
-        query = {"owner": param.persistence_target.owner_id}
+        query = {}
+        if param.persistence_target.owner_id:
+            query.update({"owner": param.persistence_target.owner_id})
+
         if param.data_id:
             query.update({"id": param.data_id})
 
@@ -217,7 +244,8 @@ class CommonDataStoreServer(datastore.DataStoreServer):
         success, size, url = self.head_object_by_key(self.calculate_s3_object_key_ex(
             self.datastore_db,
             param.persistence_target.owner_id,
-            param.persistence_target.persistence_id))
+            param.persistence_target.persistence_id,
+            obj["id"]))
 
         if not success:
             raise common.RMCError("DataStore::NotFound")
@@ -231,15 +259,70 @@ class CommonDataStoreServer(datastore.DataStoreServer):
 
         return res
 
-    # ==================================================================================
+    async def search_object(self, client, param: datastore.DataStoreSearchParam):
 
-    async def handle_change_meta(self, client, input, output):
-        datastore.logger.info("DataStoreServer.change_meta()")
-        # --- request ---
-        param = input.extract(MK8DataStoreChangeMetaParam)
-        await self.change_meta(client, param)
+        if param.result_range.size > 100:
+            raise common.RMCError("DataStore::InvalidArgument")
 
-    async def change_meta(self, client, param: MK8DataStoreChangeMetaParam):
+        # TODO: Support created/updated before/after queries
+        query = {"data_type": param.data_type}
+        cursor = self.datastore_db.find(query).skip(param.result_range.offset).limit(param.result_range.size)
+
+        if param.result_order_column == 5:  # Creation date:
+            cursor = cursor.sort("create_time", pymongo.DESCENDING)
+        elif param.result_order_column == 64:
+            cursor = cursor.sort("ratings.0.value", pymongo.DESCENDING)
+
+        objects = list(cursor)
+
+        search_result = datastore.DataStoreSearchResult()
+        search_result.total_count = len(objects)
+        search_result.total_count_type = len(objects)
+        search_result.result = []
+
+        for obj in objects:
+            meta = datastore.DataStoreMetaInfo()
+            meta.meta_binary = b''
+            meta.status = 0
+            meta.referred_count = 0
+            meta.refer_data_id = 0
+            meta.ratings = []
+
+            meta.data_id = obj["id"]
+            meta.owner_id = obj["owner"]
+            meta.name = obj["name"]
+            meta.size = obj["size"]
+            meta.data_type = obj["data_type"]
+            meta.flag = obj["flag"]
+            meta.period = obj["period"]
+            meta.tags = obj["tags"]
+
+            if param.result_option & 4:
+                meta.meta_binary = obj["meta_binary"]
+
+            meta.permission.permission = obj["access_permission"]["permission"]
+            meta.permission.recipients = obj["access_permission"]["recipients"]
+            meta.delete_permission.recipients = obj["delete_permission"]["recipients"]
+            meta.delete_permission.recipients = obj["delete_permission"]["recipients"]
+
+            meta.create_time = common.DateTime.fromtimestamp(datetime.datetime.timestamp(obj["create_time"]))
+            meta.update_time = common.DateTime.fromtimestamp(datetime.datetime.timestamp(obj["update_time"]))
+            meta.referred_time = common.DateTime.fromtimestamp(datetime.datetime.timestamp(obj["referred_time"]))
+            meta.expire_time = common.DateTime.fromtimestamp(datetime.datetime.timestamp(obj["create_time"]))
+
+            for rating in obj["ratings"]:
+                rate = datastore.DataStoreRatingInfoWithSlot()
+                rate.slot = rating["slot"]
+                rate.info.initial_value = rating["initial_value"]
+                rate.info.total_value = rating["value"]
+                rate.info.count = rating["count"]
+                meta.ratings.append(rate)
+
+            search_result.result.append(meta)
+
+        return search_result
+
+    async def change_meta(self, client, param: datastore.DataStoreChangeMetaParam):
         obj = self.datastore_db.find_one({"id": param.data_id})
         if not obj:
             raise common.RMCError("DataStore::NotFound")
@@ -260,9 +343,11 @@ class CommonDataStoreServer(datastore.DataStoreServer):
         if query != {}:
             self.datastore_db.update_one({"id": obj["id"]}, {"$set": query})
 
-    # ==================================================================================
-
     async def get_metas_multiple_param(self, client, params: list[datastore.DataStoreGetMetaParam]):
+
+        if len(params) > 100:
+            raise common.RMCError("DataStore::InvalidArgument")
+
         results = []
         infos = []
         for param in params:
@@ -301,7 +386,7 @@ class CommonDataStoreServer(datastore.DataStoreServer):
 
             obj = self.datastore_db.find_one(query)
             if not obj:
-                results.append(common.Result("DataStore::NotFound"))
+                results.append(common.Result.error("DataStore::NotFound"))
                 infos.append(meta)
 
             meta.data_id = obj["id"]
@@ -340,5 +425,65 @@ class CommonDataStoreServer(datastore.DataStoreServer):
         res = rmc.RMCResponse()
         res.results = results
         res.infos = infos
+
+        return res
+
+    async def rate_object(self, client, target: datastore.DataStoreRatingTarget, param: datastore.DataStoreRateObjectParam, fetch_ratings: bool):
+
+        obj = self.datastore_db.find_one({"id": target.data_id})
+        if not obj:
+            raise common.RMCError("DataStore::NotFound")
+
+        if len(obj["ratings"]) <= target.slot:
+            raise common.RMCError("DataStore::InvalidArgument")
+
+        rating_target = "ratings.%d" % target.slot
+        self.datastore_db.update_one({"id": target.data_id}, {
+            "$inc": {
+                rating_target + ".value": param.rating_value,
+                rating_target + ".count": 1
+            }
+        })
+
+        res = datastore.DataStoreRatingInfo()
+        res.total_value = obj["ratings"][target.slot]["value"] + param.rating_value
+        res.count = obj["ratings"][target.slot]["count"] + 1
+        res.initial_value = obj["ratings"][target.slot]["initial_value"]
+
+        return res
+
+    async def get_object_infos(self, client, object_ids: list[int]):
+        res = rmc.RMCResponse()
+        res.infos = []
+        res.results = []
+
+        for object_id in object_ids:
+
+            info = datastore.DataStoreReqGetInfo()
+            info.url = ""
+            info.headers = []
+            info.data_id = object_id
+            info.size = 0
+            info.root_ca_cert = b""
+
+            obj = self.datastore_db.find_one({"id": object_id})
+            if not obj:
+                res.infos.append(info)
+                res.results.append(common.Result.error("DataStore::NotFound"))
+                continue
+
+            success, size, url = self.head_object_by_key(self.calculate_s3_object_key_ex(
+                self.datastore_db, obj["owner"], obj["persistence_id"], obj["id"]))
+
+            if not success:
+                res.infos.append(info)
+                res.results.append(common.Result.error("DataStore::NotFound"))
+                continue
+
+            info.url = url
+            info.size = size
+
+            res.infos.append(info)
+            res.results.append(common.Result.success("DataStore::Unknown"))
 
         return res
